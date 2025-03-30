4f141086c1ddc4f273cde9ac45fb63acc6e0c3c1
Moses Natukwatsa <moses.na.43@gmail.com>
2025-03-30 17:46:16 +0300

    Commit #1
    In the New Version on line 1, 19 lines were involved in the change

---
+++ CurrencyConversion/currency-converter/src/test/java/com/currency/converter/CurrencyConverterTest.java
@@ -0,0 +1,19 @@
+package com.currency.converter;
+
+import com.exchange.rates.ExchangeRateProvider;
+import com.exchange.rates.ExchangeRateProviderImpl;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CurrencyConverterTest {
+    @Test
+    public void testConversion() {
+        ExchangeRateProvider provider = new ExchangeRateProviderImpl();
+        CurrencyConverter converter = new CurrencyConverter(provider);
+
+        assertEquals(85.0, converter.convert("USD", "EUR", 100), 0.01);
+        assertEquals(118.0, converter.convert("EUR", "USD", 100), 0.01);
+        assertEquals(75.0, converter.convert("USD", "GBP", 100), 0.01);
+        assertEquals(133.0, converter.convert("GBP", "USD", 100), 0.01);
+    }
+}
